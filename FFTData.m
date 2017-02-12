a3b1e61fa1cf969c30a0443fa6218fbc70b1da07
jej016 <joeyjenkins211@yahoo.com>
2017-02-11 18:18:14 -0600

    Working FFT unit

--- FFTData.m
+++ FFTData.m
@@ -1,12 +1,9 @@
 classdef FFTData
     methods (Static)
         function FastFourier(ch1, ch2, ch3, index)
-            fft1 = fft(ch1);
-            GraphData.graph1(fft1, index)
-            fft2 = fft(ch2);
-            GraphData.graph1(fft2, index + 1)
-            fft3 = fft(ch3);
-            GraphData.graph1(fft3, index + 2)
+            Fourier.FFourier(ch1(5000000:5005000), index)
+            Fourier.FFourier(ch2(5000000:5005000), index + 1)
+            Fourier.FFourier(ch3(5000000:5005000), index + 2)
         end
     end
 end
---
+++ Fourier.m
@@ -0,0 +1,30 @@
+classdef Fourier
+    methods (Static)
+        function FFourier(X, index)
+            Fs = 1000;            % Sampling frequency
+            T = 1/Fs;             % Sampling period
+            L = 500;        % Length of signal
+            t = (0:L-1)*T;        % Time vector
+            
+            %{
+            figure(6)
+            plot(1000*t(1:L),X(1:L))
+            title('Raw Data vs Time')
+            xlabel('t (milliseconds)')
+            ylabel('X(t)')
+            %}
+            
+            Y = fft(X);
+            P2 = abs(Y/L);
+            P1 = P2(1:L/2+1);
+            P1(2:end-1) = 2*P1(2:end-1);
+
+            figure(index)
+            f = Fs*(0:(L/2))/L;
+            plot(f,P1)
+            title('Single-Sided Amplitude Spectrum of X(t)')
+            xlabel('f (Hz)')
+            ylabel('|P1(f)|')
+        end
+    end
+end
