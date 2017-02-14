338e1d1612eaae2db34e382876356abe2d6a31f7
jaewung <no1ryu@hanmail.net>
2017-02-14 16:32:59 -0600

    Add files via upload

--- eventDetector.m
+++ eventDetector.m
@@ -3,7 +3,13 @@ classdef eventDetector
         function detector(file)       
             eventTime = xlsread(file, 'A:A');
             eventName = xlsread(file, 'B:B');
-            return table (eventTime, eventName);
+            for i = 2:length(eventTime)-1
+                eventIndex= duration(eventTime);    
+            end    
+            for j = 2 : length(evenTime)-1
+                eventIndex(j)=(eventIndex(j)+eventIndex(j-1))*1000;
+            end
+            return table (eventIndex, eventName);
         end
     end
 end
