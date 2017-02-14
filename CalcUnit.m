eb9056d9448bfa0d86f1573d8bbf1acf0076915e
Belgabad <donovankmontgomery@yahoo.com>
2017-02-14 10:39:43 -0600

    Add files via upload
    Finished Calc Unit with extra functionality. Can remove some if needed, sections are commented for explanation. Will start unit testing on this product.

--- CalcUnit.m
+++ CalcUnit.m
@@ -1,22 +1,44 @@
-classdef CalcUnit
+classdef CalcUnit %defines the class for the calc unit that will interface with the main class
     methods (Static)
-        function study(data1, data2, data3)
-            avg1 = mean(data1)
-            med1 = median(data1)
-            stdv1 = std(data1)
-            var1 = stdv1^2
+        function study(data1, data2, data3) %operations will be performed 
+            %on the three input arrays: data1, data2, data3
             
-            avg2 = mean(data2)
-            med2 = median(data2)
-            stdv2 = std(data2)
-            var2 = stdv2^2
+            %operations to be performed on each data signal will be: mean,
+            %median, standard deviation, variance, min and max
+            avgSig1 = mean(data1);
+            medSig1 = median(data1);
+            stdvSig1 = std(data1);
+            varSig1 = stdvSig1^2;
+            minSig1 = min(data1);
+            maxSig1 = max(data1);
             
-            avg3 = mean(data3)
-            med3 = median(data3)
-            stdv3 = std(data3)
-            var3 = stdv3^2
+            avgSig2 = mean(data2);
+            medSig2 = median(data2);
+            stdvSig2 = std(data2);
+            varSig2 = stdvSig2^2;
+            minSig2 = min(data2);
+            maxSig2 = max(data2);
             
-            save('data.mat', 'avg1', 'med1', 'stdv1', 'var1', 'avg2', 'med2', 'stdv2', 'var2', 'avg3', 'med3', 'stdv3', 'var3');
+            avgSig3 = mean(data3);
+            medSig3 = median(data3);
+            stdvSig3 = std(data3);
+            varSig3 = stdvSig3^2;
+            minSig3 = min(data3);
+            maxSig3 = max(data3);
+            
+            %operations to be performed on all data signals as a whole
+            dataAll = [data1; data2; data3];
+            
+            avgAll = mean(dataAll);
+            medAll = median(dataAll);
+            stdvAll = std(dataAll);
+            varAll = stdvAll^2;
+            minAll = min(dataAll);
+            maxAll = max(dataAll);
+            
+            %each variable will be saved to a .mat file to be able to be
+            %referenced later
+            save('t1.mat', 'avgSig1', 'medSig1', 'stdvSig1', 'varSig1', 'minSig1', 'maxSig1', 'avgSig2', 'medSig2', 'stdvSig2', 'varSig2', 'minSig2', 'maxSig2', 'avgSig3', 'medSig3', 'stdvSig3', 'varSig3', 'minSig3', 'maxSig3', 'avgAll', 'medAll', 'stdvAll', 'varAll', 'minAll', 'maxAll');
         end
     end
 end
